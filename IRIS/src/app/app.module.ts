330b326292580a7bd959c4faa4a826add4bafbda
Diab <emailcodydiab@gmail.com>
2020-09-13 08:51:07 -0500

    succeful api call

--- IRIS/src/app/app.module.ts
+++ IRIS/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
-
+import {RouterModule} from '@angular/router';
+import {allAppRoutes} from './routes';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     WeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(allAppRoutes),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- IRIS/src/app/weather/weather.component.ts
+++ IRIS/src/app/weather/weather.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormBuilder, FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent implements OnInit {
+  public weatherSearchForm:FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.weatherSearchForm = this.formBuilder.group({
+      location: ['']
+    });
   }
-
+   sendToOpenWeatherAPI(formValues) {
+     console.log(formValues);
+     var currentUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ formValues.location + "&appid=858b3927a8044ecb64a2c351fd3d6fad&units=imperial"
+   
+     fetch(currentUrl)
+      .then(function(response) {
+          if (response.ok) {
+              response.json().then(function(data){
+                 
+                 console.log(data)
+                  
+                 
+                 
+                //fetch for future weather using coord from data 
+                 var futureUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+data.coord.lat+"&lon=" +data.coord.lon+ "&appid=858b3927a8044ecb64a2c351fd3d6fad&units=imperial"
+                 fetch(futureUrl)
+                  .then(function(response){
+                      if(response.ok){
+                          response.json().then(function(data){
+                              
+                             console.log(data)
+                              
+                          })
+            
+                          
+                      }
+                  })
+                 
+              })
+          }else{
+             alert("Error: " + response.statusText);
+          }
+ 
+   })
+  }
 }
